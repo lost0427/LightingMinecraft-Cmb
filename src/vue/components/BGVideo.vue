0d32d09d3e04e6c67db0764ff829f721fd6838ca
z1 <aaronzyg@163.com>
2025-04-05 11:37:04 +0800

    Pics Update

--- src/vue/components/BGVideo.vue
+++ src/vue/components/BGVideo.vue
@@ -4,7 +4,9 @@
 
 <template>
     <video class="absolute" style="object-fit: cover;" preload="auto" loop disableremoteplayback disablepictureinpicture autoplay playsinline muted>
+        <source src="https://cdn.jsdelivr.net/gh/lost0427/LightingMinecraft-Cmb@main/src/assets/HomePage/Museum.webm" type="video/webm"/>
         <source src="../../assets/HomePage/Museum.webm" type="video/webm"/>
+        <source src="https://cdn.jsdelivr.net/gh/lost0427/LightingMinecraft-Cmb@main/src/assets/HomePage/Museum.mp4" type="video/mp4"/>
         <source src="../../assets/HomePage/Museum.mp4" type="video/mp4"/>
     </video>
 </template>
